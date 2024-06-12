d8eba2513fb4eda4b39ef97dd75c0285541e0a3b
simone menichetti <simon3menichettui95@gmail.com>
2024-06-12 15:41:16 +0200

    build:(add files to start Pari e Dispari) Feat(add UserInteract Prompt to JS)

--- dispari/JS/mainjavascript.js
+++ dispari/JS/mainjavascript.js
@@ -1,7 +1,16 @@
-// Palidroma
-// Chiedere all’utente di inserire una parolaù
+ // PARI E DISPARI!!
+
+//  Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
 
 
-let InputUtente = prompt()
-// Creata una funzione per capire se la parola inserita è palindroma nel mainfunctio.js
+// L’utente sceglie pari o dispari 
+let sceltaUtenteParioDispari= prompt("Scegli pari o dispari");
+// L'utente inserisce un numero da 1 a 5.
+let numeroUtente = prompt("Scegli un numero da 1 a 5")
+
+
 
--- myjsfunction/mainfunction.js
+++ myjsfunction/mainfunction.js
@@ -39,3 +39,8 @@ if (palindroma(InputUtente)) {
     //         console.log(parolaInversa);
 
    
+
+
+
+                            // PARI E DISPARI!!
+
